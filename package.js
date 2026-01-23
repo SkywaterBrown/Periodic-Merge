41de9516c5b1c79ea80cd2bafa090b7d377c9639
SkywaterBrown <skywater.brown@protonmail.com>
2026-01-23 11:51:10 -0500

    Update package.js

--- package.js
+++ package.js
@@ -2,15 +2,7 @@
   "name": "element-merge-game",
   "version": "1.1.0",
   "description": "Element Merge Game with Global Leaderboards",
-  "main": "game_logic.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
   "dependencies": {
     "@neondatabase/serverless": "^0.10.0"
-  },
-  "devDependencies": {},
-  "engines": {
-    "node": ">=18.0.0"
   }
 }
